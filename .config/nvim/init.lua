4f4e11a1d107b42c08fa6ebf437578f319e2bea5
fl0r3k <pflorczy@gmail.com>
2024-09-20 09:25:39 +0200

    Added lookml files support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -215,6 +215,8 @@ require("lazy").setup({
 		end,
 	},
 
+	{ "chrismaher/vim-lookml" },
+
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 	--
 	-- This is often very useful to both group configuration, as well as handle
